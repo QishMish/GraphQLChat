137626fa683dd334897f8a840e94ca1477fc65d6
QishMish <mishikoqajaia50@gmail.com>
2022-08-02 21:58:36 +0400

    Display current user profile and exlude logged in user from chat users list

--- client/src/components/chatSidebarHeader/Index.jsx
+++ client/src/components/chatSidebarHeader/Index.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useAuthContext } from '../../context';
 
-
-import styles from './styles.module.css'
+import styles from './styles.module.css';
 
 const ChatSidebarHeader = () => {
+  const {
+    userState: { user },
+  } = useAuthContext();
   return (
     <div className={styles.chatSidebarHeader}>
-        <img width={"25px"} className={styles.avatar} src="https://cdn-icons-png.flaticon.com/512/147/147142.png" alt="avatar" />
-        <div className={styles.content}>
-            <h2>Mike Smith</h2>
-            <span>@Mike</span>
-        </div>
+      <img
+        width={'25px'}
+        className={styles.avatar}
+        src='https://cdn-icons-png.flaticon.com/512/147/147142.png'
+        alt='avatar'
+      />
+      <div className={styles.content}>
+        <h2>{user.username}</h2>
+        <span>{user.email}</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatSidebarHeader
+export default ChatSidebarHeader;
